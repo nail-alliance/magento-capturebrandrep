72d3935727e1027e1487c4fbb79d559931d9bf45
ralenaNA <ralena@nailalliance.com>
2022-11-29 11:26:20 -0600

    Rework JS to use try/catch

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -1,8 +1,19 @@
 window.onload = () => {
   let docLoc = document.URL;
-  let trimmedLoc = docLoc.replace(
-    "https://gelishmorgantaylor.co.uk/customer/account/create/#",
-    ""
-  );
-  document.getElementById("brand_rep_hash").value = trimmedLoc;
+  let brHash = document.getElementById("brand_rep_hash");
+  try {
+    let trimmedLoc = docLoc.replace(
+      "https://gelishmorgantaylor.co.uk/customer/account/create/#",
+      ""
+    );
+    if (
+      trimmedLoc != "https://gelishmorgantaylor.co.uk/customer/account/create/"
+    ) {
+      brHash.value = trimmedLoc;
+    } else {
+      brHash.value = "";
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
