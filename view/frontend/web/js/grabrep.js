bb2ad8e5da5e8fb872f9febf159d25c0ef806d83
ralenaNA <ralena@nailalliance.com>
2023-02-21 16:45:31 -0600

    Add onload back to JS

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -1,12 +1,15 @@
-let docLoc = document.URL;
-let brHash = document.getElementById("brand_rep_hash");
-try {
-  let trimmedLoc = docLoc.replace(docLoc + "/#", "");
-  if (trimmedLoc != docLoc + "/#") {
-    brHash.value = trimmedLoc;
-  } else {
-    brHash.value = "";
+window.onload = () => {
+  let docLoc = document.URL;
+  let brHash = document.getElementById("brand_rep_hash");
+  try {
+    let trimmedLoc = docLoc.replace(docLoc + "/#", "");
+    if (trimmedLoc != docLoc + "/#") {
+      brHash.value = trimmedLoc;
+    } else {
+      brHash.value = "";
+    }
+    console.log("rep", docLoc);
+  } catch (error) {
+    console.log(error);
   }
-} catch (error) {
-  console.log(error);
-}
+};
