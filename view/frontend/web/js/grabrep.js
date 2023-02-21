5af5ca96f8ff8518b06fdc48c1c7635ce2727025
ralenaNA <ralena@nailalliance.com>
2023-02-21 16:29:41 -0600

    Fix redundant code

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -1,38 +1,12 @@
-window.onload = () => {
-  let docLoc = document.URL;
-  let brHash = document.getElementById("brand_rep_hash");
-  try {
-    if (docLoc == "https://gelishmorgantaylor.co.uk/customer/account/create/") {
-      let trimmedLoc = docLoc.replace(
-        "https://gelishmorgantaylor.co.uk/customer/account/create/#",
-        ""
-      );
-      if (
-        trimmedLoc !=
-        "https://gelishmorgantaylor.co.uk/customer/account/create/"
-      ) {
-        brHash.value = trimmedLoc;
-      } else {
-        brHash.value = "";
-      }
-    } else if (
-      docLoc == "https://nailtechsupply.com/customer/account/create/"
-    ) {
-      let trimmedLoc = docLoc.replace(
-        "https://nailtechsupply.com/customer/account/create/#",
-        ""
-      );
-      if (
-        trimmedLoc !=
-        "https://gelishmorgantaylor.co.uk/customer/account/create/"
-      ) {
-        brHash.value = trimmedLoc;
-      } else {
-        brHash.value = "";
-      }
-      console.log("Rep", docLoc, trimmedLoc);
-    }
-  } catch (error) {
-    console.log(error);
+let docLoc = document.URL;
+let brHash = document.getElementById("brand_rep_hash");
+try {
+  let trimmedLoc = docLoc.replace(docLoc + "/#", "");
+  if (trimmedLoc != docLoc + "/#") {
+    brHash.value = trimmedLoc;
+  } else {
+    brHash.value = "";
   }
-};
+} catch (error) {
+  console.log(error);
+}
