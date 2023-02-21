5f5d7714215613aa87bb258b3579e6abcdd5d9a5
ralenaNA <ralena@nailalliance.com>
2023-02-21 16:06:41 -0600

    Add NTS to logic

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -2,16 +2,35 @@ window.onload = () => {
   let docLoc = document.URL;
   let brHash = document.getElementById("brand_rep_hash");
   try {
-    let trimmedLoc = docLoc.replace(
-      "https://gelishmorgantaylor.co.uk/customer/account/create/#",
-      ""
-    );
-    if (
-      trimmedLoc != "https://gelishmorgantaylor.co.uk/customer/account/create/"
+    if (docLoc == "https://gelishmorgantaylor.co.uk/customer/account/create/") {
+      let trimmedLoc = docLoc.replace(
+        "https://gelishmorgantaylor.co.uk/customer/account/create/#",
+        ""
+      );
+      if (
+        trimmedLoc !=
+        "https://gelishmorgantaylor.co.uk/customer/account/create/"
+      ) {
+        brHash.value = trimmedLoc;
+      } else {
+        brHash.value = "";
+      }
+    } else if (
+      docLoc == "https://nailtechsupply.com/customer/account/create/"
     ) {
-      brHash.value = trimmedLoc;
-    } else {
-      brHash.value = "";
+      let trimmedLoc = docLoc.replace(
+        "https://nailtechsupply.com/customer/account/create/#",
+        ""
+      );
+      if (
+        trimmedLoc !=
+        "https://gelishmorgantaylor.co.uk/customer/account/create/"
+      ) {
+        brHash.value = trimmedLoc;
+      } else {
+        brHash.value = "";
+      }
+      console.log("Rep", docLoc, trimmedLoc);
     }
   } catch (error) {
     console.log(error);
