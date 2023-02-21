09d50dddfcc26eed5aafc189b5cebfb570326181
ralenaNA <ralena@nailalliance.com>
2023-02-21 17:21:59 -0600

    Modify code to work global

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -2,13 +2,14 @@ window.onload = () => {
   let docLoc = document.URL;
   let brHash = document.getElementById("brand_rep_hash");
   try {
-    let trimmedLoc = docLoc.replace(docLoc + "/#", "");
-    if (trimmedLoc != docLoc + "/#") {
-      brHash.value = trimmedLoc;
+    let trimmedLoc = docLoc.split("#");
+
+    if (trimmedLoc[1] != docLoc + "/#") {
+      brHash.value = trimmedLoc[1];
     } else {
       brHash.value = "";
     }
-    console.log("rep", docLoc);
+    console.log("rep", brHash.value, trimmedLoc[1]);
   } catch (error) {
     console.log(error);
   }
