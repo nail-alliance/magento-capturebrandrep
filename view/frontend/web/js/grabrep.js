8c39cb9261867ec5b0e0f8723502ee5192d854e9
ralenaNA <ralena@nailalliance.com>
2023-10-26 13:52:15 -0500

    Remove clutter from grabrep.js

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -1,29 +1,3 @@
-// window.onload = () => {
-//   let docLoc = document.URL;
-//   let brHash = document.getElementById("brand_rep_hash");
-
-//   if (localStorage.getItem("brand_rep") !== undefined) {
-//     brHash.value = localStorage.getItem("brand_rep");
-//   }
-
-//   try {
-//     let trimmedLoc = docLoc.split("#");
-
-//     if (trimmedLoc[1] != docLoc) {
-//       brHash.value = trimmedLoc[1];
-
-//       if (brHash.value !== undefined) {
-//         localStorage.setItem("brand_rep", trimmedLoc[1]);
-//       }
-//     } else {
-//       brHash.value = "";
-//     }
-
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 window.onload = () => {
   let docLoc = document.URL;
   let brHash = document.getElementById("brand_rep_hash");
