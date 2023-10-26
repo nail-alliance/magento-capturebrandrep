dfd500931bf705c5cb80b16074b2afddcc1123b6
ralenaNA <ralena@nailalliance.com>
2023-10-26 12:59:47 -0500

    Rework module to use localStorage instead of URL based captures

--- view/frontend/web/js/grabrep.js
+++ view/frontend/web/js/grabrep.js
@@ -1,16 +1,55 @@
+// window.onload = () => {
+//   let docLoc = document.URL;
+//   let brHash = document.getElementById("brand_rep_hash");
+
+//   if (localStorage.getItem("brand_rep") !== undefined) {
+//     brHash.value = localStorage.getItem("brand_rep");
+//   }
+
+//   try {
+//     let trimmedLoc = docLoc.split("#");
+
+//     if (trimmedLoc[1] != docLoc) {
+//       brHash.value = trimmedLoc[1];
+
+//       if (brHash.value !== undefined) {
+//         localStorage.setItem("brand_rep", trimmedLoc[1]);
+//       }
+//     } else {
+//       brHash.value = "";
+//     }
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 window.onload = () => {
   let docLoc = document.URL;
   let brHash = document.getElementById("brand_rep_hash");
+ 
   try {
-    let trimmedLoc = docLoc.split("#");
-
-    if (trimmedLoc[1] != docLoc) {
-      brHash.value = trimmedLoc[1];
-    } else {
-      brHash.value = "";
-    }
-    console.log("rep", brHash.value, trimmedLoc[1]);
+     let trimmedLoc = docLoc.split("#");
+ 
+     if (trimmedLoc[1] != docLoc) {
+       
+       brHash.value = trimmedLoc[1];
+ 
+       if(brHash.value == trimmedLoc[1]) {
+         localStorage.setItem("brand_rep", trimmedLoc[1]);
+       }
+ 
+     } else {
+       brHash.value = "";
+     }
+ 
+     let storedBrandRep = localStorage.getItem("brand_rep");
+ 
+     if (storedBrandRep !== null && storedBrandRep !== undefined) {
+       brHash.value = storedBrandRep;
+     }
+ 
   } catch (error) {
-    console.log(error);
+     console.log(error);
   }
-};
+ };
